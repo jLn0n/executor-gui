59770359e441dffb0cdd924e71c64e491ccc2160
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-11-02 19:35:01 +0800

    made a bruh moment

--- src/loader.lua
+++ src/loader.lua
@@ -1,5 +1,5 @@
 getgenv().repoImport = function(path, branch)
-	return loadstring(game:HttpGetAsync(script.format("https://raw.githubusercontent.com/jLn0n/executor-gui/%s/%s", branch or "main", path)), path)
+	return loadstring(game:HttpGetAsync(string.format("https://raw.githubusercontent.com/jLn0n/executor-gui/%s/%s", branch or "main", path)), path)
 end
 
 repoImport("src/main.lua")()
